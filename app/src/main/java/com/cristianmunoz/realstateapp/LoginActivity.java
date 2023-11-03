e1be95970241c2adce367d0e39317c511212975f
B13 <kllanos@unab.edu.co>
2023-11-03 09:32:45 -0500

    3/11/2023

--- app/src/main/java/com/cristianmunoz/realstateapp/LoginActivity.java
+++ app/src/main/java/com/cristianmunoz/realstateapp/LoginActivity.java
@@ -61,7 +61,7 @@ public class LoginActivity extends AppCompatActivity {
                             Toast.makeText(LoginActivity.this, "Inicio de sesión exitoso",
                                     Toast.LENGTH_SHORT).show();
 
-                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            Intent intent = new Intent(LoginActivity.this, WelcomeActivity.class);
                             startActivity(intent);
                             finish();
                         } else {
